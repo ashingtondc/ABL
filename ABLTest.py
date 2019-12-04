ed57a4406cacff986c027afcf0f6a1ee47cbd982
Ashok Parasa <ashingtondc@gmail.com>
2019-12-03 21:02:29 -0500

    Added Unique IDs test

--- ABLTest.py
+++ ABLTest.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import main
-from parse import parseData
+from parse import parseData, extract_nums
 import unittest
 
 
@@ -59,5 +59,53 @@ class TestClock(unittest.TestCase):
         data = parseData("test_clock.txt")
 
         self.assertEqual(20, data['clock']['total']) """
+
+class TestCMDVars(unittest.TestCase):
+    def test_set(self):
+        args = "-r 2000 -t 0.5 -p 10 -c 15 -v 25 -pp 25 -cp 75 -vp 30 -ps 1.2 -cs 6.2 -vs 10.2 -pd 0 -cd 0 -vd 0 -d 0 -sd 1"
+        os.system("python main.py " + args + " > test_vars.txt")
+        data = parseData("test_vars.txt")
+
+        nums = extract_nums(args)
+        vals = list(data.values())
+        for i in range(len(nums)):
+            self.assertEqual(nums[i], vals[i+1])
+    # Passes... Commented out since it takes forever
+    """ def test_default(self):
+        defaults = [1000, 20, 30, 20, 50, 50, 50, 50, 1.3411, 5.3645, 11.176, 1, 1, 1, 0, 0]
+        os.system("python main.py > test_vars.txt")
+        data = parseData("test_vars.txt")
+
+        vals = list(data.values())
+        for i in range(len(defaults)):
+            self.assertEqual(defaults[i], vals[i+1]) """
+
+# Also tests road user length
+class TestUniqueIDs(unittest.TestCase):
+    def test_set(self):
+        os.system("python main.py -t 0.2 -r 500 > test_IDs.txt") 
+        data = parseData("test_IDs.txt")
+        users = data['road_users']['list']
+        last = None
+        count = 0
+        for user in users:
+            if last == None:
+                last = user['id']
+            else:
+                self.assertEqual(user['id'], last+1)
+                last = user['id']
+            count += 1
+
+            # Test length of users
+            if user['type'] == "Pedestrian":
+                self.assertEqual(1, user['length'])
+            if user['type'] == "Bicycle":
+                self.assertEqual(2, user['length'])
+            if user['type'] == "Motor Vehicle":
+                self.assertEqual(6, user['length'])
+        
+        self.assertEqual(count, data['road_users']['totals']['total'])
+        self.assertEqual(count, last+1)
+
 if __name__ == '__main__': 
     unittest.main() 
