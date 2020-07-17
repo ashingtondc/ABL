355c2559d13e0a82bdbbe8394e6a78d284713249
Ashok Parasa <ashingtondc@gmail.com>
2020-07-17 18:34:34 +0000

    added light parse mode

--- Graphing/parse.py
+++ Graphing/parse.py
@@ -237,6 +237,175 @@ def parseData(filename):
 
     return info
 
+
+def parseDataLite(filename):
+    info = {}
+    with open(filename, 'r') as file:
+        print(filename)
+        line = file.readline()
+        line = file.readline()
+        line = file.readline()
+        line = file.readline()
+        line = line.split()
+        info['start_time'] = line[3] + " " + line[4]
+        line = file.readline()
+        line = file.readline()
+        line = file.readline()
+        # Command line parameter values
+        while not line.isspace():
+            param = re.search("^[^:]+", line)
+            param = param.group().replace(" ", "_")
+            param = param.lower()
+            val = re.search("-?\d+\.?\d*", line)
+            val = float(val.group())
+            info[param] = val
+            line = file.readline()
+        
+        line = file.readline()
+        # Road info
+        line = file.readline()
+        vals = extract_nums(line)
+        road = {
+            "west_end": vals[0],
+            "east_end": vals[1],
+            "total": vals[2]
+        }
+        info['road'] = road
+        # Clock info
+        line = file.readline()
+        vals = extract_nums(line)
+        clock = {
+            "start": vals[0],
+            "end": vals[1],
+            "tick_length": vals[2],
+            "total": vals[3]
+        }
+        info['clock'] = clock
+        # Duration of extended interactions
+        line = file.readline()
+        vals = extract_nums(line)
+        info['interaction_time'] = float(vals[0])
+        # Road user sources
+        line = file.readline()
+        vals = extract_nums(line)
+        num = int(vals[0])
+        sources = []
+        for i in range(num):
+            source = {}
+            line = file.readline()
+            vals = extract_nums(line)
+            line = line.split()
+            # direction may have '<' in it
+            source['direction'] = line[0].replace("<", "")
+            source['type'] = line[1]
+            source['location'] = vals[0]
+            source['generating'] = vals[1]
+            source['speed'] = vals[2]
+            source['speed_dist'] = vals[3]
+            source['length'] = vals[4]
+            source['min_gap'] = vals[5]
+            sources.append(source)
+        info['sources'] = sources
+        file.readline()
+        line = file.readline()
+        # Duration
+        info['duration'] = extract_nums(line)[0]
+        file.readline()
+        # Interactions
+        file.readline()
+        unaggregated = extract_nums(file.readline())[0]
+        recorded = extract_nums(file.readline())[0]
+        file.readline()
+        line = file.readline()
+        interactions = []
+        while not line.isspace():
+            line = file.readline()
+        info['unaggregated'] = unaggregated
+        info['recorded'] = recorded
+        info['interactions'] = interactions
+
+        # Road Users
+        road_users = {}
+        # "Road Users Log"
+        line = file.readline() 
+        # "Motor"
+        line = file.readline() 
+        # "Vehicles..."
+        line = file.readline()
+        # EastBound
+        line = file.readline()
+        vals = extract_nums(line)
+        eastbound = {}
+        eastbound['motor_vehicles'] = vals[0]
+        eastbound['bicycles'] = vals[1]
+        eastbound['peds'] = vals[2]
+        eastbound['total'] = vals[3]
+        road_users['eastbound'] = eastbound
+        # WestBound
+        line = file.readline()
+        vals = extract_nums(line)
+        westbound = {}
+        westbound['motor_vehicles'] = vals[0]
+        westbound['bicycles'] = vals[1]
+        westbound['peds'] = vals[2]
+        westbound['total'] = vals[3]
+        road_users['westbound'] = westbound
+        # Totals
+        line = file.readline()
+        vals = extract_nums(line)
+        totals = {}
+        totals['motor_vehicles'] = vals[0]
+        totals['bicycles'] = vals[1]
+        totals['peds'] = vals[2]
+        totals['total'] = vals[3]
+        road_users['totals'] = totals
+        # Blank
+        line = file.readline()
+        # List
+        line = file.readline()
+        users = []
+        for i in range(int(totals['total'])):
+            line = file.readline()
+        road_users['list'] = users
+        info['road_users'] = road_users
+        # Interaction totals
+        vals = extract_nums(line)
+        info['total_interactions'] = vals[0]
+        info['vru_interactions'] = vals[1]
+        info['mvxmv_interactions'] = vals[2]
+        # Skip redundant interaction info
+        for i in range(4):
+            file.readline()
+        # Last line: Simulation finish times
+        line = file.readline().split()
+        info['end_time'] = line[3] + " " + line[4]
+        info['prog_duration'] = float(line[9])
+
+        # Calculate MVxMVxP
+        # Calculate MVxMVxB
+        ped = 0
+        bike = 0
+        for intxn in interactions:
+            M = 0
+            P = 0
+            B = 0
+            for letter in intxn['code']:
+                if letter == 'M' or letter == 'm':
+                    M += 1
+                elif letter == 'P' or letter == 'p':
+                    P += 1
+                elif letter == 'B' or letter == 'b':
+                    B += 1
+            if M >= 2:
+                if P > 0:
+                    ped += 1
+                elif B > 0:
+                    bike += 1
+        info['MV_MV_B'] = bike
+        info['MV_MV_P'] = ped
+
+    return info
+
 def boolean(item):
     return item == "True"
 
